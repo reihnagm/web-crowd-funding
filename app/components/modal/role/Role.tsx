bb16248828da74ab00f71dfe702a50a7f1435377
Reihan Agam <reihanagam7@gmail.com>
2025-06-30 10:46:23 +0700

    update

--- app/components/modal/role/Role.tsx
+++ app/components/modal/role/Role.tsx
@@ -41,7 +41,10 @@ const RoleModal: React.FC<RoleModalProps> = ({ open, onClose }) => {
           </button>
         )}
         <button
-          onClick={onClose}
+          onClick={() => {
+            onClose();
+            setStep("select");
+          }}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 text-2xl z-10"
         >
           ×
