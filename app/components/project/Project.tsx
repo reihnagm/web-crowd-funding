abe227318290da5bd873948150363eb93d2824cb
Ryuzaki Shin <rudinhoer160@gmail.com>
2025-07-04 10:28:10 +0700

    add list project

--- app/components/project/Project.tsx
+++ app/components/project/Project.tsx
@@ -1,13 +1,15 @@
+import { IProjectData } from "@/app/interface/IProject";
+import { formatPriceOrEmpty, priceLib } from "@/app/lib/price";
 import { useRouter } from "next/navigation";
 
-export const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
+export const ProjectCard: React.FC<{ project: IProjectData }> = ({ project }) => {
   const router = useRouter();
 
   const bgColor =
-    project.statusColor === "purple"
+    project.is_approved
       ? "bg-purple-900 text-purple-800"
       : "bg-green-700 text-green-700";
-  const isFinish = project.status === "Proyek Berjalan" ? "block" : "hidden";
+  const isFinish = project.is_apbn ? "block" : "hidden";
 
   return (
     <div
@@ -19,25 +21,30 @@ export const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
       <div className="relative h-40">
         <img
           src={
-            project.medias.length != 0
+            project.medias.length !== 0
               ? project.medias[0].path
               : "/images/img.jpg"
           }
-          alt={project.alt}
+          alt={project.title}
           className="object-cover w-full h-full"
+          onError={(e) => {
+            const target = e.target as HTMLImageElement;
+            target.onerror = null; // mencegah infinite loop
+            target.src = "/images/img.jpg";
+          }}
         />
         <div
           className={`absolute inset-0 ${
-            project.statusColor === "purple" ? "bg-purple-900" : "bg-green-700"
+            project.is_apbn ? "bg-purple-900" : "bg-green-700"
           } bg-opacity-60`}
         />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <span
             className={`bg-white ${bgColor} text-xs font-bold px-4 py-1 rounded-full shadow`}
           >
-            {project.status}
+            {project.goal}
           </span>
-        </div>
+        </div> */}
       </div>
       <div className="p-4 bg-gray-100 h-full">
         <p className="font-semibold text-sm text-start mb-2">{project.title}</p>
@@ -50,28 +57,28 @@ export const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
             <div className="relative w-[80%] h-4 bg-purple-200 rounded-full my-2">
               <div
                 className="absolute top-0 left-0 h-4 bg-[#3E268D] rounded-full"
-                style={{ width: "100%" }}
+                style={{ width: "80%" }}
               ></div>
               <span className="absolute right-[-4px] top-1/2 -translate-y-1/2 translate-x-full bg-green-500 text-white text-xs font-bold px-2 rounded-full shadow">
-                100%
+                80%
               </span>
             </div>
           </li>
           <li className="flex justify-between">
             <span>Kebutuhan Modal</span>
-            <span>{project.modal}</span>
+            <span>{formatPriceOrEmpty((project?.nominal_value ?? 0).toString() ?? "", "id-ID", "IDR")}</span>
           </li>
           <li className="flex justify-between">
             <span>Minimal Investasi</span>
-            <span>{project.minInvest}</span>
+            <span>{formatPriceOrEmpty(1000000, "id-ID", "IDR")}</span>
           </li>
           <li className="flex justify-between">
             <span>Jangka Waktu</span>
-            <span>{project.periode}</span>
+            <span>{project.time_periode}</span>
           </li>
           <li className="flex justify-between">
             <span>Proyeksi ROI</span>
-            <span>{project.roi}</span>
+            <span>18% p.a</span>
           </li>
         </ul>
       </div>
